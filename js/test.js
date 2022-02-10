f119fc55ac26fa4b731a43d5f933c6171d502338
Christopher Vigliotti <christopher@christophervigliotti.com>
2022-02-10 06:42:35 -0800

    verifying that you cannot fire ajax calls without running your page in a web server (Chrome prevents it)

--- js/test.js
+++ js/test.js
@@ -2,6 +2,8 @@ $(function() { // jquery ready
     $( "#submit_button" ).on( "click", function(event) { // submit button click listener
         var first_name = document.getElementById('first_name').value; // get this value using traditional js
         var last_name = $('#last_name').val(); // get this value using jquery
+        var ajax_url = 'controller/controller.cfc?method=doForm';
+        // ajax_url = 'README.md'; // this works
         if(first_name.length == 0 || last_name.length == 0){
             var alert_string = '';
             if(first_name.length == 0){
@@ -14,7 +16,7 @@ $(function() { // jquery ready
         }else{
             $.ajax({
                 type:"POST",
-                url:"controller/controller.cfc?method=doForm",
+                url:ajax_url,
                 data: {
                     first_name: first_name,
                     last_name: last_name
